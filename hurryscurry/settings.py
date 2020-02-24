ddecde95af273d3ae121d518fb4bc5e23a8bc31f
carthick <challakarthik@gmail.com>
2020-02-24 14:29:07 -0500

    project settings

--- hurryscurry/settings.py
+++ hurryscurry/settings.py
@@ -1,5 +1,6 @@
 import environ
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,3 +117,5 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
