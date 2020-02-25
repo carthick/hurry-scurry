d79c4a01585efe699aede15ea11b91281b2f1651
carthick <challakarthik@gmail.com>
2020-02-25 11:23:51 -0500

    redirect login

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import CustomRegistrationForm
 from django.contrib import messages
 
@@ -9,7 +9,7 @@ def register(request):
         if register_form.is_valid():
             register_form.save()
             messages.success(request,"New user created")
-            return redirect('register')
+            return redirect('login')
     else:
         register_form = CustomRegistrationForm()
     return render(request,'register.html', {'register_form':register_form})
