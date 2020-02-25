a2fccb914938ef12fef138d3790d7df78f43c6fa
carthick <challakarthik@gmail.com>
2020-02-25 09:59:59 -0500

    static files

--- hurryscurry/settings.py
+++ hurryscurry/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "todolist"
@@ -121,5 +123,4 @@ LOGIN_URL = "login"
 
 django_heroku.settings(locals())
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+
